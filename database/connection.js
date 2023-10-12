c96d3b417e5f8603e5e0df8c07672fd91650daf7
Michael <eclipselunar20182018@gmail.com>
2023-10-12 17:40:39 -0500

    coneccion

--- database/connection.js
+++ database/connection.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-
 const connection = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI);
@@ -9,7 +8,6 @@ const connection = async () => {
         throw new Error("No se pudo conectar a la base de datos");
     }
 }
-
 module.exports = {
     connection
 }
