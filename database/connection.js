a40ba9eb00cf1c7074bdd5595fbda6667ea09211
Michael <eclipselunar20182018@gmail.com>
2023-10-12 17:49:48 -0500

    asd

--- database/connection.js
+++ database/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const connection = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
+        await mongoose.connect("mongodb+srv://1524:1524@serverlessinstanceapp99.8ztdeie.mongodb.net/app_ase");
         console.log("base de datos conectada");
     } catch (error) {
         console.log(error);
